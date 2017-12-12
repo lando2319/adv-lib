fd610c80adfbc6d9440c43fe770393d7f65ba9b0
lando2319 <mikepland@gmail.com>
2017-12-12 00:05:22 -0600

    updates log

--- doStuff.js
+++ doStuff.js
@@ -2,6 +2,7 @@ function advStuff() {
     console.log("really ADV");
     console.log("really REALLY ADV");
     console.log("really REALLY ADV2222");
+    console.log("really REALLY ADV2226");
 }
 
 module.exports.advStuff = advStuff;
